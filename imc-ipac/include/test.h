b982f820295da72ccf4814488bf3613844c8a238
Tregan <basrops@outlook.com>
2013-01-28 05:45:49 -0800

    IMC-IPAC source folder + Added folder for our own sources + Added test files, delete when programming

---
+++ imc-ipac/include/test.h
@@ -0,0 +1,13 @@
+/*
+ * test.h
+ *
+ *  Created on: 28 jan. 2013
+ *      Author: Bas
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+
+
+#endif /* TEST_H_ */
---
+++ imc-ipac/source/test.c
@@ -0,0 +1,8 @@
+/*
+ * test.c
+ *
+ *  Created on: 28 jan. 2013
+ *      Author: Bas
+ */
+
+
